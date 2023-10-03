b1dc5627c4e25ba9023d12b6d74d12080dbf8856
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-10-03 22:41:41 +0200

    Data.sql, maven

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO car_model (brand, model, color)
+VALUES ('Polonez', '300', 'White');
+
+INSERT INTO car_model (brand, model, color)
+VALUES ('Opel', 'Astra', 'Red');
+
+INSERT INTO car_model (brand, model, color)
+VALUES ('Toyota', 'Camry', 'Blue');
