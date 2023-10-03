f760db7ba3092c237a9693338a84cd86d96f2d55
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-10-03 22:40:24 +0200

    Car model

---
+++ src/main/java/com/example/cars/model/CarModel.java
@@ -0,0 +1,22 @@
+package com.example.cars.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CarModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String brand;
+    private String model;
+    private String color;
+}
